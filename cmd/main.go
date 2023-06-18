dba6099b09730b6a52fb7ff682517e0a6421ee20
Cadeusept <matwey.likhota@yandex.ru>
2023-06-18 22:33:41 +0300

    added config

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,26 @@ import (
 	"github.com/Cadeusept/notes-app/pkg/handler"
 	"github.com/Cadeusept/notes-app/pkg/repository"
 	"github.com/Cadeusept/notes-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	webSrv := new(webServer.Server)
-	if err := webSrv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := webSrv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error running web server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("..\\configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
