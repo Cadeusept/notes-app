26bdc57ba2ed86bcb6b1628bf2afe5730bf48490
Cadeusept <matwey.likhota@yandex.ru>
2023-06-18 17:21:41 +0300

    added templates for repository and services

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 
 	webServer "github.com/Cadeusept/notes-app"
 	"github.com/Cadeusept/notes-app/pkg/handler"
+	"github.com/Cadeusept/notes-app/pkg/repository"
+	"github.com/Cadeusept/notes-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	webSrv := new(webServer.Server)
 	if err := webSrv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error running web server: %s", err.Error())
